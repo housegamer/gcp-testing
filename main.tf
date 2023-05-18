5341264064d484e43040b6d592fb94bf15fe8b15
housegamer <rodri123106@gmail.com>
2023-05-18 13:39:53 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ terraform {
 provider "google" {
   project     = var.project_id
   region      = var.region
-  credentials = var.GOOGLE_APPLICATION_CREDENTIALS
+  credentials = var.credentials
 }
 
 resource "google_service_account" "sa_default" {
