29af497cd696a99a2d2ac43eeb48b93ef83d122d
rocastillo <rocastillo@deloitte.com>
2025-04-30 18:05:11 -0500

    testing logic with script

--- targetValidation.bash
+++ targetValidation.bash
@@ -18,3 +18,6 @@ while [api -eq 0]; do
   fi
 done
 echo 'creating releases...'
+
+gcloud deploy releases create 'app-${_BACKEND}-release-${SHORT_SHA}' --delivery-pipeline test-cd-pipeline --region $LOCATION --images=$_BACKEND=$LOCATION-docker.pkg.dev/$PROJECT_ID/$_ARTIFACT_REPO/$_BACKEND:v.0.1.${SHORT_SHA} --from-k8s-manifest=./kubernetes/app.yaml
+gcloud deploy releases create 'app-${_FRONTEND}-release-${SHORT_SHA}' --delivery-pipeline test-cd-pipeline --region $LOCATION --images=$_FRONTEND=$LOCATION-docker.pkg.dev/$PROJECT_ID/$_ARTIFACT_REPO/$_FRONTEND:v.0.2.${SHORT_SHA} --from-k8s-manifest=./kubernetes/frontend.yaml
